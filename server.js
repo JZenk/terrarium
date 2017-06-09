939d280c454bf3a04b79b7f4ea2b2212c68e2401
JZenk <chestylarue@gmail.com>
2017-06-08 22:33:02 -0500

    Serve static assets

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express')
   , app = express()
+  , path = require('path')
 
-app.use(express.static(__dirname + '/view'));
+app.use('/view', express.static('view'));
 //Store all HTML files in view folder.
-app.use(express.static(__dirname + '/script'));
+app.use('/script', express.static('script'));
 //Store all JS and CSS in Scripts folder.
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
@@ -11,7 +12,7 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 
 app.get('/', function (req, res, next) {
   try {
-    res.sendFile('index.html')
+    res.sendFile(path.join(__dirname +'/view/index.html'))
   } catch (e) {
     next(e)
   }
